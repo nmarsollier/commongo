cbb745e9f3cbb60c29c962377453a4fe680d4d68
Nestor Marsollier <nmarsollier@gmail.com>
2024-12-28 14:43:05 -0300

    mongo

--- test/mktools/mongo.go
+++ test/mktools/mongo.go
@@ -1,7 +1,7 @@
 package mktools
 
 import (
-	"github.com/golang/mock/gomock"
+	gomock "github.com/golang/mock/gomock"
 	"github.com/nmarsollier/commongo/test/mockgen"
 )
 
@@ -23,4 +23,13 @@ func ExpectInsertOneError(coll *mockgen.MockCollection, err error, times int) {
 
 func ExpectInsertOne(coll *mockgen.MockCollection, times int) {
 	coll.EXPECT().InsertOne(gomock.Any(), gomock.Any()).Return("", nil).Times(times)
+}
+
+func ExpectFindOne[T any](coll *mockgen.MockCollection, userData *T, times int) {
+	coll.EXPECT().FindOne(gomock.Any(), gomock.Any(), gomock.Any()).DoAndReturn(
+		func(arg1 interface{}, params interface{}, update *T) error {
+			*update = *userData
+			return nil
+		},
+	).Times(times)
 }
