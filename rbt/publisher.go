c4c90270699fb7eb1aa2ae07421fe135424dc706
Nestor Marsollier <nmarsollier@gmail.com>
2024-12-29 13:22:58 -0300

    Update publisher.go

--- rbt/publisher.go
+++ rbt/publisher.go
@@ -8,6 +8,7 @@ import (
 )
 
 type RabbitPublisher[T any] interface {
+	PublishForResult(data T, exchange string, routingKey string) error
 	PublishTo(exchange string, routingKey string, data T) error
 	Publish(data T) error
 }
@@ -64,7 +65,15 @@ func (c *rabbitPublisher[T]) Publish(data T) error {
 	return c.PublishTo(c.exchangeName, c.routingKey, data)
 }
 
+func (c *rabbitPublisher[T]) PublishForResult(data T, exchange string, routingKey string) error {
+	return c.publish(c.exchangeName, c.routingKey, data, exchange, routingKey)
+}
+
 func (c *rabbitPublisher[T]) PublishTo(exchange string, routingKey string, data T) error {
+	return c.publish(exchange, routingKey, data, "", "")
+}
+
+func (c *rabbitPublisher[T]) publish(exchange string, routingKey string, data T, fbExchange string, fbRoutingKey string) error {
 	err := c.exchangeDeclare()
 	if err != nil {
 		c.log.Error(err)
@@ -73,6 +82,8 @@ func (c *rabbitPublisher[T]) PublishTo(exchange string, routingKey string, data 
 
 	send := publishMessage[T]{
 		CorrelationId: c.log.CorrelationId(),
+		Exchange:      fbExchange,
+		RoutingKey:    fbRoutingKey,
 		Message:       data,
 	}
 
@@ -103,5 +114,7 @@ func (c *rabbitPublisher[T]) PublishTo(exchange string, routingKey string, data 
 
 type publishMessage[T any] struct {
 	CorrelationId string `json:"correlation_id" example:"123123" `
+	Exchange      string `json:"exchange" example:"cart"`
+	RoutingKey    string `json:"routing_key" example:""`
 	Message       T      `json:"message" example:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbklEIjoiNjZiNjBlYzhlMGYzYzY4OTUzMzJlOWNmIiwidXNlcklEIjoiNjZhZmQ3ZWU4YTBhYjRjZjQ0YTQ3NDcyIn0.who7upBctOpmlVmTvOgH1qFKOHKXmuQCkEjMV3qeySg" `
 }
